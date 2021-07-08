e4aab4bc86a9bea9b32e6dd4eb87a3f75d613737
pedoncadmin <45769583+pedoncadmin@users.noreply.github.com>
2021-07-08 16:01:03 -0400

    Update ImageComputer.ps1

--- Code/PowerShell/ImageComputer.ps1
+++ Code/PowerShell/ImageComputer.ps1
@@ -1,7 +1,7 @@
 #
 # Windows Deployment Imaging Script
 #
-# Copyright (C) 2020 Curtis Glavin, Jonathan Huppi, Robert Burkey
+# Copyright (C) 2021 Curtis Glavin, Jonathan Huppi, Joshua Casiano
 #
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License Version 3 as published by the Free Software Foundation.
 #
@@ -17,14 +17,6 @@
 # 59 Temple Place, Suite 330
 # Boston, MA 02111-1307, USA
 #
-
-#
-# By default, round numbers will be used for partition sizes.
-# If this value is false, the script will try to create
-# specific partition sizes, in bytes (the specific byte
-# sizes can be edited in the script below).
-#
-$simpleDiskGeometry = $true
 
 #
 # Will contain number of disk to install to
@@ -47,14 +39,10 @@ $peDrive = "*"
 $windowsDrive = "*"
 
 #
-# Will indicate EFI/BIOS, 64/32 Bit
+# Will contain System drive letter
 #
-$systemType = "*"
+$systemDrive = "*"
 
-#
-# Switch to high performance mode to accelerate process
-#
-#powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
 $disks = Get-Disk
 #
@@ -70,14 +58,14 @@ if (!$disks -or (($disks | Where-Object -FilterScript {$_.Size -gt 0} | measure)
 
 #
 # Obtain letter of PE drive
-# (educated guess based on location of Images\Windows.swm)
+# (educated guess based on location of Images\Windows21H1ProEFI64SysprepWindows.swm)
 # and select an available drive letter for the new
 # Windows partition that will be created
 #
 $originalDrives = (Get-PSDrive -PSProvider FileSystem).Name
 if ($originalDrives) {
     ForEach ($drive in $originalDrives) {
-        if (Test-Path "${drive}:\Images\Windows2004ProEFI64SysprepWindows.swm") {
+        if (Test-Path "${drive}:\Images\Windows21H1ProEFI64SysprepWindows.swm") {
             $peDrive = $drive
             break
         }
@@ -90,13 +78,14 @@ if ($peDrive -eq "*") {
     Exit
 }
 $availableLetters = [char[]]([int][char]'A'..[int][char]'Z') | Where {$originalDrives -NotContains $_}
-if (!$availableLetters) {
+if ($availableLetters.Length -lt 2) {
     Clear-Host
-    Write-Host "No drive letters available.`n`n"
+    Write-Host "Insufficient drive letters available.`n`n"
     [void](Read-Host 'Press Enter to quit the program...')
     Exit
 }
 $windowsDrive = $availableLetters[-1]
+$systemDrive = $availableLetters[-2]
 
 #
 # Prompt for a disk to install to
@@ -169,78 +158,27 @@ if ($name -eq "*") {
     until($name -ne "*")
 }
 
-#
-# Ask if the system will be EFI or BIOS, 32 or 64 bit
-#
-$options = @()
-$options += New-Object PsObject -Property @{ Option=1; Description="EFI 64 Bit"}
-$options += New-Object PsObject -Property @{ Option=2; Description="EFI 32 Bit"}
-$options += New-Object PsObject -Property @{ Option=3; Description="BIOS 64 Bit"}
-$options += New-Object PsObject -Property @{ Option=4; Description="BIOS 32 Bit"}
-do {
-    Clear-Host
-    Write-Host "Select the type of system to install`n`n"
-    $options | Format-Table -Property Option,Description
-    $input = Read-Host "Enter Option number of system type"
-    if (1..4 -contains $input) {
-        $systemType = $options[[int]$input - 1].Description
-    }
-} while ($systemType -eq "*")
 Clear-Host
 
 #
 # Setup disk to receive image
 #
 Clear-Disk -Number $disk -RemoveData -RemoveOEM -Confirm:$false -ErrorAction SilentlyContinue | Out-Null
-switch ($systemType.SubString(0,4)) {
-    "EFI " {
-        Initialize-Disk $disk -PartitionStyle GPT | Out-Null
-        if ($simpleDiskGeometry) {
-            New-Partition -DiskNumber $disk -Size 100MB -GptType "{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}" | Format-Volume -FileSystem FAT32 | Out-Null
-            New-Partition -DiskNumber $disk -Size 16MB -GptType "{e3c9e316-0b5c-4db8-817d-f92df00215ae}" | Out-Null
-            New-Partition -DiskNumber $disk -DriveLetter $windowsDrive -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Windows" | Out-Null
-            $originalSize = (Get-PartitionSupportedSize -DiskNumber $disk -PartitionNumber 3).SizeMax
-            Resize-Partition -DiskNumber $disk -PartitionNumber 3 -Size ($originalSize - 506MB)
-            New-Partition -DiskNumber $disk -Size 505MB -GptType "{de94bba4-06d1-4d40-a16a-bfd50179d6ac}" | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Recovery" | Out-Null
-        } else {
-            New-Partition -DiskNumber $disk -Size 104857600 -GptType "{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}" | Format-Volume -FileSystem FAT32 | Out-Null
-            New-Partition -DiskNumber $disk -Size 16777216 -GptType "{e3c9e316-0b5c-4db8-817d-f92df00215ae}" | Out-Null
-            New-Partition -DiskNumber $disk -DriveLetter $windowsDrive -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Windows" | Out-Null
-            $originalSize = (Get-PartitionSupportedSize -DiskNumber $disk -PartitionNumber 3).SizeMax
-            Resize-Partition -DiskNumber $disk -PartitionNumber 3 -Size ($originalSize - 506MB)
-            New-Partition -DiskNumber $disk -Size 529530880 -GptType "{de94bba4-06d1-4d40-a16a-bfd50179d6ac}" | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Recovery" | Out-Null
-        }
-    }
-    "BIOS" {
-        Initialize-Disk $disk -PartitionStyle MBR | Out-Null
-        if ($simpleDiskGeometry) {
-            New-Partition -DiskNumber $disk -Size 550MB -Isactive | Format-Volume -FileSystem NTFS -NewFileSystemLabel "System Reserved" | Out-Null
-        } else {
-            New-Partition -DiskNumber $disk -Size 575668224 -Isactive | Format-Volume -FileSystem NTFS -NewFileSystemLabel "System Reserved" | Out-Null
-        }
-        New-Partition -DiskNumber $disk -DriveLetter $windowsDrive -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Windows" | Out-Null
-    }
-}
+
+Initialize-Disk $disk -PartitionStyle GPT | Out-Null
+New-Partition -DiskNumber $disk -Size 314572800 -DriveLetter $systemDrive -GptType "{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}" | Format-Volume -FileSystem FAT32 | Out-Null
+New-Partition -DiskNumber $disk -Size 16777216 -GptType "{e3c9e316-0b5c-4db8-817d-f92df00215ae}" | Out-Null
+New-Partition -DiskNumber $disk -DriveLetter $windowsDrive -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Windows" | Out-Null
+$originalSize = (Get-PartitionSupportedSize -DiskNumber $disk -PartitionNumber 3).SizeMax
+Resize-Partition -DiskNumber $disk -PartitionNumber 3 -Size ($originalSize - 555MB)
+New-Partition -DiskNumber $disk -Size 550MB -GptType "{de94bba4-06d1-4d40-a16a-bfd50179d6ac}" | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Recovery" | Out-Null
 Write-Host "Completed configuration of destination disk"
 
 #
 # Clone image to destination disk
 #
-switch ($systemType) {
-    "EFI 64 Bit" {
-        dism /apply-image /imagefile:"${peDrive}:\Images\Windows2004ProEFI64SysprepWindows.swm" /swmfile:"${peDrive}:\Images\Windows2004ProEFI64SysprepWindows*.swm" /index:1 /applydir:"${windowsDrive}:\"
-    }
-    "EFI 32 Bit" {
-        dism /apply-image /imagefile:"${peDrive}:\Images\WindowsEFI32.swm" /swmfile:"${peDrive}:\Images\WindowsEFI32*.swm" /index:1 /applydir:"${windowsDrive}:\"
-    }
-    "BIOS 64 Bit" {
-        dism /apply-image /imagefile:"${peDrive}:\Images\WindowsBIOS64.swm" /swmfile:"${peDrive}:\Images\WindowsBIOS64*.swm" /index:1 /applydir:"${windowsDrive}:\"
-    }
-    "BIOS 32 Bit" {
-        dism /apply-image /imagefile:"${peDrive}:\Images\WindowsBIOS32.swm" /swmfile:"${peDrive}:\Images\WindowsBIOS32*.swm" /index:1 /applydir:"${windowsDrive}:\"
-    }
-}
-iex "${windowsDrive}:\Windows\System32\bcdboot ${windowsDrive}:\Windows"
+dism /apply-image /imagefile:"${peDrive}:\Images\Windows21H1ProEFI64SysprepWindows.swm" /swmfile:"${peDrive}:\Images\Windows21H1ProEFI64SysprepWindows*.swm" /index:1 /applydir:"${windowsDrive}:\"
+iex "${windowsDrive}:\Windows\System32\bcdboot ${windowsDrive}:\Windows /s ${systemDrive}: /f UEFI"
 Write-Host "Applied image to destination disk"
 
 #
